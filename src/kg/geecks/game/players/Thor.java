55a3ccd642664079274ca9235de1ac00d758cab2
Nurmuhamed2003 <bakybaevnurmuhamed@gmail.com>
2023-02-26 23:21:25 +0600

    Initial commit

---
+++ src/kg/geecks/game/players/Thor.java
@@ -0,0 +1,22 @@
+package kg.geecks.game.players;
+
+import kg.geecks.game.logic.RPG_Game;
+
+public class Thor  extends Hero{
+    boolean thorStan;
+    public Thor(int health, int damage,  String name) {
+        super(health, damage, SuperAbility.STAN, name);
+    }
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+        thorStan = RPG_Game.random.nextBoolean();
+        if (thorStan){
+            boss.setDamage(0);
+            System.out.println("Boss оглушен на 1 раунд");
+        }
+        else {
+            boss.setDamage(50);
+        }
+    }
+}
